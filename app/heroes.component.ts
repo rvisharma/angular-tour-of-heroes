e2ec6482147e4273f8dd899d31c010acda4be784
Ravi Sharma <rvisharma91@gmail.com>
2016-09-26 09:36:16 +0530

    Initial commit. Services Chapter completed

---
+++ app/heroes.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
+
+@Component({
+    selector: 'my-heroes',
+    styleUrls: ['app/heroes.component.css'],
+    templateUrl: 'app/heroes.component.html'
+})
+
+export class HeroesComponent implements OnInit {
+    selectedHero: Hero;
+    heroes: Hero[];
+
+    constructor(
+        private heroService: HeroService,
+        private router:Router
+    ) {
+        console.log('AppComponent init')
+    }
+
+    ngOnInit() {
+        this.getHeroes();
+        console.log('AppComponent.OnInit called');
+    }
+
+    onSelect = (hero: Hero): void => {
+        this.selectedHero = hero;
+    };
+
+    getHeroes = (): void => {
+        console.log('AppComponent.getHeroes called');
+        this.heroService
+            .getHeroes()
+            .then(heroes => this.heroes = heroes);
+    };
+
+    gotoDetail = (): void => {
+        let link = ['/detail', this.selectedHero.id];
+        this.router.navigate(link);
+    };
+
+}
